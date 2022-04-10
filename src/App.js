e8e0b8e05513811145262f65e3f06fd6c61132c0
Karolina Hamerszmidt <karolina@hamerszmidt.eu>
2022-04-10 19:33:30 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./she-codes.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <img src={logo} className="App-logo img-fluid" alt="logo" />
+    </div>
+  );
+}
+
+export default App;
